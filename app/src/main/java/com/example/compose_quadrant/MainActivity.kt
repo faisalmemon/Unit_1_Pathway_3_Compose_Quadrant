db826d1348b71bd50d465d3a5a473ca07f2beae3
Faisal Memon <faisalmemonuk@yahoo.com>
2024-06-25 18:44:55 +0100

    Add font styling and padding.

--- app/src/main/java/com/example/compose_quadrant/MainActivity.kt
+++ app/src/main/java/com/example/compose_quadrant/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.compose_quadrant.ui.theme.Compose_QuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +28,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Compose_QuadrantTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    ComposeCard(
+                        title = stringResource(id = R.string.text_composable),
+                        longDescription = stringResource(id = R.string.text_composable_description),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ComposeCard(
+    title: String,
+    longDescription: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(vertical = 16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = longDescription,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Compose_QuadrantTheme {
-        Greeting("Android")
+        ComposeCard(
+            title = stringResource(id = R.string.text_composable),
+            longDescription = stringResource(id = R.string.text_composable_description)
+        )
     }
 }
